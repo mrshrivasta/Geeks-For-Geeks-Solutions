dfc7a0ae360fdae8e3a331a463915b8edbf8b89c
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-30 18:24:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.py
@@ -0,0 +1,14 @@
+class Solution:
+    def floorSqrt(self, n):
+        low, high = 1, n
+        ans = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid <= n:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return ans
