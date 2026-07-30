48c313c01d736b5bd9c037486eb71c80f92be05b
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-07-30 18:20:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+        if n == 2:
+            return True
+        if n % 2 == 0:
+            return False
+
+        i = 3
+        while i * i <= n:
+            if n % i == 0:
+                return False
+            i += 2
+
+        return True
