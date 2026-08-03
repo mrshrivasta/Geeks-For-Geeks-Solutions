fa7b113e2c2d75ecba9136b1aa252234257c6271
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-08-03 13:33:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, s):
+        n = len(s)
+        start = 0
+        max_len = 1
+
+        for i in range(n):
+            l = r = i
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r - l + 1 > max_len:
+                    start = l
+                    max_len = r - l + 1
+                l -= 1
+                r += 1
+
+            l, r = i, i + 1
+            while l >= 0 and r < n and s[l] == s[r]:
+                if r - l + 1 > max_len:
+                    start = l
+                    max_len = r - l + 1
+                l -= 1
+                r += 1
+
+        return s[start:start + max_len]
