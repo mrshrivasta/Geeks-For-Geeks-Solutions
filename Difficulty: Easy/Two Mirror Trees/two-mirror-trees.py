00b817715eaf8dba97708130528ce922265e27e6
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-08-06 11:25:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Mirror Trees/two-mirror-trees.py
@@ -0,0 +1,13 @@
+class Solution:
+    def areMirror(self, root1, root2):
+        if not root1 and not root2:
+            return True
+
+        if not root1 or not root2:
+            return False
+
+        return (
+            root1.data == root2.data and
+            self.areMirror(root1.left, root2.right) and
+            self.areMirror(root1.right, root2.left)
+        )
