d90f62ab2160f13a4a58d75a3f8fbcf814873323
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-08-06 22:48:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, mat, x):
+        n = len(mat)
+        m = len(mat[0])
+
+        low, high = 0, n * m - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            val = mat[mid // m][mid % m]
+
+            if val == x:
+                return True
+            elif val < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return False
