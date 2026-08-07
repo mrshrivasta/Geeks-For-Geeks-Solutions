993ee766a3a24edeefb4b06682a76c1fc9b0d144
Karanam Shrivasta <shrivastakaranam@gmail.com>
2026-08-07 15:19:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Heap Sort/heap-sort.py
@@ -0,0 +1,25 @@
+class Solution:
+    def heapify(self, arr, n, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < n and arr[left] > arr[largest]:
+            largest = left
+
+        if right < n and arr[right] > arr[largest]:
+            largest = right
+
+        if largest != i:
+            arr[i], arr[largest] = arr[largest], arr[i]
+            self.heapify(arr, n, largest)
+
+    def heapSort(self, arr):
+        n = len(arr)
+
+        for i in range(n // 2 - 1, -1, -1):
+            self.heapify(arr, n, i)
+
+        for i in range(n - 1, 0, -1):
+            arr[0], arr[i] = arr[i], arr[0]
+            self.heapify(arr, i, 0)
